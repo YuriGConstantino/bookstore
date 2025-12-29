bfa34793c756702af752e688a318390db819aa06
YuriGConstantino <yuri.gconstantino@gmail.com>
2025-12-29 10:05:11 -0300

    feat: changing tests

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from product.models import Product
-from product_serializer.catetegory_serializer import CategorySerializer
+from product.serializers.category_serializer import CategorySerializer
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(required=True, many=True)
